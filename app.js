c500c6dd46cd8d834853159e11045bc42b0294bc
Som Hunka <somhunka@gmail.com>
2026-01-19 19:35:42 +0530

    Fix Render port binding

--- app.js
+++ app.js
@@ -123,6 +123,9 @@ app.use((err, req, res, next) => {
 });
 
 // Starting the server
-app.listen(8080, () => {
-  console.log("server is listening on port 8080");
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
 });
+
